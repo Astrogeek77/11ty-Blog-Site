970b8a96f81ec720b1c03ca8f89c978eaae2953c
Gautam Jain <astrogeek77@gmail.com>
2021-07-18 21:16:11 +0530

    eleventy config additions

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const { DateTime } = require('luxon')
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter('postDate', (dataObj) => {
+        return DateTime.fromJSDate(dataObj).toLocaleString(DateTime.DATE_MED)
+    })
 
     return {
         dir: {
